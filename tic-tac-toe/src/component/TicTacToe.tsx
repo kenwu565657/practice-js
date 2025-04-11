83d84b23601488e90ec7d50dd7383742658178c6
kenwu565657 <kenwu565656@icloud.com>
2025-04-11 23:49:03 +0800

    add game logic

--- tic-tac-toe/src/component/TicTacToe.tsx
+++ tic-tac-toe/src/component/TicTacToe.tsx
@@ -1,6 +1,6 @@
 import { use, useState } from "react";
 import Information from "./Information";
-import Squares from "./Squares";
+import Squares from "./Square/Squares";
 import RestartButton from "./RestartButton";
 import SwitchMode from "./SwitchMode";
 
@@ -23,11 +23,22 @@ const TicTacToe = () => {
     const {winnerId, winnerStepsList, lastStepsToWin} = judgmentInfo;
     const isGameEndedInTie: boolean = players.flatMap((playerId) => playersStepMap.get(playerId)).length === 9;
 
+    const handleClickSquare: (squareId: number) => void = (squareId: number) => {
+        const isSquareEnable = playersStepMap.get(1)?.indexOf(squareId) === -1 && playersStepMap.get(-1)?.indexOf(squareId) === -1;
+        if (isSquareEnable) {
+            const prevPlayerStepList: number[] = playersStepMap.get(currentPlayerId) || [];
+            const newPlayerStepList: number[] = [...prevPlayerStepList, squareId];
+            const newPlayerStepMap: Map<number, number[]> = new Map(playersStepMap).set(currentPlayerId, newPlayerStepList);
+            setPlayerStepMap(newPlayerStepMap);
+            setCurrentPlayerId((prev: number) => -1 * prev);
+        };
+    };
+
     return (
         <div className="background">
             <div className="container">
                 <Information currentPlayerId={currentPlayerId} winnerId={winnerId} isGameEndedInTie={isGameEndedInTie}></Information>
-                <Squares playersStepMap={playersStepMap} winnerStepList={winnerStepsList} handleClickSquare={}></Squares>
+                <Squares playersStepMap={playersStepMap} winnerStepList={winnerStepsList} handleClickSquare={handleClickSquare}></Squares>
                 <div className="action">
                     <RestartButton handleResetState={}></RestartButton>
                     <SwitchMode isActive={isSinglePlay} handleSwitchPlayMode={}></SwitchMode>
